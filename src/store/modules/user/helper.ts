645633dcabdd2e817a03641bdbf4755c6def68ef
李猛 <limeng.b@mininglamp.com>
2023-03-23 18:57:03 +0800

    profile

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/kense-lab/chatgpt-web/main/src/assets/avatar2.jpg',
+      name: '回旋的阿姆斯特朗',
+      description: '塔塔开',
     },
   }
 }
